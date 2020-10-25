5c5d33599739c0e00b384d522ea6336241fa72e1
Kevin Gutierrez G <kevingtzz@outlook.com>
2020-10-25 14:20:45 -0500

    feat: multiconnection server and client done.

---
+++ test/multi_connection_server.py
@@ -0,0 +1,58 @@
+#!/usr/bin/env pyton3
+
+import sys
+import selectors
+import socket
+import types
+
+sel = selectors.DefaultSelector()
+
+if len(sys.argv) != 3:
+    print("usage:", sys.argv[0], "<host> <port>")
+    sys.exit(1)
+
+host, port = sys.argv[1], int(sys.argv[2])
+lsock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+lsock.bind((host, port))
+lsock.listen()
+
+print('listening on', (host, port))
+
+lsock.setblocking(False) #Call made to this socket will no longer block.
+sel.register(lsock, selectors.EVENT_READ, data=None)
+
+def service_connection(key, mask):
+    sock = key.fileobj
+    data = key.data
+    if mask & selectors.EVENT_READ:
+        recv_data = sock.recv(1024)
+        if recv_data:
+            data.outb += recv_data
+        else:
+            print('closing connection to:', data.addr)
+            sel.unregister(sock)
+            sock.close()
+    if mask & selectors.EVENT_WRITE:
+        if data.outb:
+            print('echoing', repr(data.outb), 'to', data.addr)
+            sent = sock.send(data.outb)
+            data.outb = data.outb[sent:]
+
+def accept_wrapper(sock):
+    conn, addr = sock.accept()  # Should be ready to read
+    print('accepted connection from', addr)
+    conn.setblocking(False)
+    data = types.SimpleNamespace(addr=addr, inb=b'', outb=b'')
+    events = selectors.EVENT_READ | selectors.EVENT_WRITE
+    sel.register(conn, events, data=data)
+
+while True:
+    events = sel.select(timeout = None)
+    for key, mask in events:
+        if key.data is None:
+            accept_wrapper(key.fileobj)
+        else:
+            service_connection(key, mask)
+
+
+
